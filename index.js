30b1ae163543b1fc3259f93d6dd5c1a66119d7bf
Purushothaman <purushothaman9148@gmail.com>
2023-01-07 17:08:24 +0530

    Modified Code

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const PORT = process.env.PORT || 8000;
+app.use(express.json())
 
 //connection 
-mongoose.connect(process.env.MONGO_URI).then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server Running on Port : ${PORT}`)
-    })
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => { })
+
+app.get('/', (req, res) => {
+    res.send("Home Page");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server Running on Port : ${PORT}`)
 })
